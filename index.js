61d22486a25920e9c8caac7e12f923a48ef8edeb
mwb86 <masonwbrown@gmail.com>
2017-01-12 08:59:16 -0500

    not working

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 var mongoose = require('mongoose');
 var Schema = require("./db/schema.js");
+
+function findFood(name){
+  Restaurant.findOne({name: restaurant}, function(err, result){
+    if(err){
+      console.log(err);
+    }
+    else{
+      console.log(result);
+    }
+  });
+}
+function findByZip(zipcode){
+  return Restaurant.find(zipcode);
+}
