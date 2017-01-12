2962e6aa443bfd12ac89a1a8195678db35035054
mconnors333 <matthew.connors333@gmail.com>
2017-01-12 09:02:29 -0500

    Connors Week 09 Day 03 HW

--- db/schema.js
+++ db/schema.js
@@ -1,4 +1,37 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
+
 var db = mongoose.connection;
+
 db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once("open", () => {
+  console.log("\n********************\n");
+  console.log("Matt Connors is the Greatest Database Connector!");
+  console.log("\n********************\n");
+});
+
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+
+var MenuSchema = new Schema({
+  name: String,
+  age: Number,
+  reminders: [{type: Schema.ObjectId, ref: "Reminder"}]
+});
+
+var RestaurantSchema = new Schema({
+  name: String,
+  address: {
+    street: String,
+    zipcode: Number
+  },
+  yelpUrl: String,
+  items: [MenuSchema]
+});
+
+
+module.exports - {
+
+
+};
