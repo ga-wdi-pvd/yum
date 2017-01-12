1a53285ef3553c2679db5c930429745d7396d26e
awanderlyss <alyssa.felix7@gmail.com>
2017-01-12 09:00:42 -0500

    Add response

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,36 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once("open", () => {
+    console.log("database has been connected!");
+});
+
+var Schema = mongoose.Schema;
+
+// menu item schema
+var MenuItemSchema = new Schema({
+  title:String
+});
+
+// restaurant schema
+var RestaurantSchema = new Schema({
+  name:String,
+  address: {
+    street:String,
+    zipcode:Number
+  },
+  yelpUrl:String,
+  menuItems:[{type: Schema.ObjectId, ref:"MenuItem"}]
+});
+
+// models
+var Restaurant = mongoose.model("Restaurant", RestaurantSchema);
+var MenuItem = mongoose.model("MenuItem", MenuItemSchema);
+
+
+// export models
+module.export = {
+  Restaurant:Restaurant,
+  MenuItem:MenuItem
+};
--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,19 @@
 var Schema = require("../db/schema.js");
+
+var Restaurant = Schema.Restaurant;
+var Item = Schema.Item;
+
+
+Restaurant.remove({}, err=>{
+  if(err){
+    console.log(err);
+  }
+});
+
+MenuItem.remove({}, err=>{
+  if(err){
+    console.log(err);
+  }
+});
+
+var gracies = new Restaurant({name: "Gracie's", address: {street: "194 Washington St", zipcode: 02903 }, yelp: "https://www.yelp.com/biz/gracies-providence", })
