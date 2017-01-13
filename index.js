972ff8d6976106de3fa056be62a73e31fdb3b00b
yourUsername <your_email@example.com>
2017-01-13 09:07:39 -0500

    updated yum

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 var mongoose = require('mongoose');
-var Schema = require("./db/schema.js");
+var Schema = require("./mongoose/connection.js");
+var express = require('express');
+var hbs = require('hbs');
+var parser = require("body-parser");
+
+var restaurant = Schema.Restaurant;
+var menuItem = Schema.MenuItem
+
+mongoose.connect("mongodb:/localhost/yum");
+
+app.get('/', (req, res) => {
+  res.render('app-welcome');
+});
+
+//Create
+app.post('/restaurants', (req, res) => {
+
+});
+
+app.set('view engine', 'hbs');
+app.set('views', './views');
+
+app.use(express.static(__dirname + '/public'));
+
+//Port Set-up
+app.listen(3001, function(){
+  console.log("******LISTENING ON PORT 3001******");
+});
