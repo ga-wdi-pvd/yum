3030295bd2430be5556dc9341e45fc3ab001f7c7
“neilburd” <neilburd@gmail.com>
2017-01-13 07:37:17 -0500

    getting mongoose running

--- db/seeds.js
+++ db/seeds.js
@@ -1,52 +1,52 @@
-var Schema = require("../db/schema.js");
+var mongoose = require('connection.js');
+var seedData = require('seeds.json');
+mongoose.Promise = global.Promise;
+var RestaurantModel = mongoose.model("Restaurant", RestaurantSchema);
 
-var Restaurant = Schema.Restaurant;
-var MenuItem = Schema.MenuItem;
+RestaurantModel.remove({}).then(function(){
+  RestaurantModel.collection.insert(seedData).then(function(){
 
-Restaurant.remove({}, err => {
-  if(err){
-    console.log(err);
-  }
-});
-
-MenuItem.remove({}, err => {
-  if (err){
-    console.log(err);
-  }
-});
-///DB WAS ABLE TO BE CREATED BEFOR I ADDED THE FOLLOWING CODE
-var italianPlace = new Restaurant({ name: "Italian Shack", address: {street: "Main St.", zipCode: 02689},yelpUrl: "yelp.com"});
-
-var mexicanPlace = new Restaurant({ name: "Mexican Casa", address: {street: "North Main St.",zipCode: 02689},yelpUrl: "yelp.com"});
-
-var pasta = new MenuItem({ title: "Penne with Sauce"});
-var pasta2 = new MenuItem({ title: "Riginotni with Sauce"});
-var taco = new MenuItem({ title: "Beef Taco"});
-var taco2 = new MenuItem({ title: "Chicken Taco"});
-
-var restaurants = [italianPlace, mexicanPlace];
-var menuItems = [pasta, pasta2, taco, taco2];
-
-restaurants.forEach((rest, i) => {
-  rest.items.push( menuItems[i], menuItems[i + 1] )
-  rest.save((err, restaurants) => {
-    if (err){
-      console.log(err);
-    } else {
-      console.log(restaurants);
-    }
-  })
+    process.exit();
+  });
 });
 
 
-
-// for (var i = 0; i < restaurants.length; i++){
-//   restaurants[i].menuItems.push(menuItems[i], menuItems[i + 1])
-//   restaurants[i].save((err, restaurants) => {
+// var Schema = require("../db/schema.js");
+//
+// var Restaurant = Schema.Restaurant;
+// var MenuItem = Schema.MenuItem;
+//
+// Restaurant.remove({}, err => {
+//   if(err){
+//     console.log(err);
+//   }
+// });
+//
+// MenuItem.remove({}, err => {
+//   if (err){
+//     console.log(err);
+//   }
+// });
+// ///DB WAS ABLE TO BE CREATED BEFOR I ADDED THE FOLLOWING CODE
+// var italianPlace = new Restaurant({ name: "Italian Shack", address: {street: "Main St.", zipCode: 02689},yelpUrl: "yelp.com"});
+//
+// var mexicanPlace = new Restaurant({ name: "Mexican Casa", address: {street: "North Main St.",zipCode: 02689},yelpUrl: "yelp.com"});
+//
+// var pasta = new MenuItem({ title: "Penne with Sauce"});
+// var pasta2 = new MenuItem({ title: "Riginotni with Sauce"});
+// var taco = new MenuItem({ title: "Beef Taco"});
+// var taco2 = new MenuItem({ title: "Chicken Taco"});
+//
+// var restaurants = [italianPlace, mexicanPlace];
+// var menuItems = [pasta, pasta2, taco, taco2];
+//
+// restaurants.forEach((rest, i) => {
+//   rest.items.push( menuItems[i], menuItems[i + 1] )
+//   rest.save((err, restaurants) => {
 //     if (err){
 //       console.log(err);
 //     } else {
 //       console.log(restaurants);
 //     }
 //   })
-// };
+// });
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var mongoose = require('mongoose');
-// mongoose.connect("mongodb://localhost/restaurants");
+var hbs       = require("hbs");
+var Restaurant = require('./db/connection');
+var express   = require("express");
+var mongoose  = require('mongoose');
 var db = mongoose.connection;
+
+// mongoose.connect("mongodb://localhost/restaurants");
 
 db.on("error", err => {
   console.log(err);
@@ -9,6 +13,7 @@ db.on("error", err => {
 db.once("open", () => {
   console.log("database has been connected!");
 })
+
 var Schema = require("./db/schema.js");
 
 app.listen(3000, function(){
