b41309c73eb091c745d1aea76f0de60f2365a754
patrickthesailorman <patrickthesaiorman@gmail.com>
2017-01-13 08:34:42 -0500

    not working

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,40 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once("open", () => {
+  console.log("database has been connected!");
+});
+
+var Schema = mongoose Schema;
+  ObjectId = Schema.ObjectId;
+
+var MenuItemsSchema = mongoose.Schema ({
+  title: String,
+});
+
+var RestaurantSchema = mongoose.Schema ({
+  name: String,
+  address: { street: String, state: String, zipcode: Number },
+  yelpUrl: String,
+  items: [MenuItemsSchema]
+});
+
+
+
+var RestaurantModel = mongoose.model("Restaurant", RestaurantSchema);
+var MenuItemModel = mongoose.model("MenuItemSchema", MenuItemSchema);
+
+Restaurant.create({ name: "Sora", address: "16 Donovan st."}, (err, restaurant) => {
+  if (err){
+    console.log(err);
+  }
+  else{
+    console.log(restaurant);
+  }
+});
+
+module.exports = {
+  Restaurant: RestaurantModel,
+  MenuItem: MenuItemModel
+};
