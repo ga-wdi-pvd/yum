db3320d08c4f46cd7109fe3930b42984abf6d8b5
mconnors333 <matthew.connors333@gmail.com>
2017-01-13 08:06:37 -0500

    Connors Week 09 Day 04 HW

--- db/schema.js
+++ db/schema.js
@@ -1,15 +1,4 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/yum');
-
-var db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-
-db.once("open", () => {
-  console.log("\n********************\n");
-  console.log("Matt Connors is the Greatest Database Connector!");
-  console.log("\n********************\n");
-});
 
 var Schema = mongoose.Schema;
 var ObjectId = Schema.ObjectId;
@@ -30,8 +19,8 @@ var RestaurantSchema = new Schema({
   items: [MenuSchema]
 });
 
+mongoose.model("Restaurant", RestaurantSchema);
+mongoose.model("Menu", MenuSchema);
+mongoose.connect("mongodb://localhost/yum");
 
-module.exports - {
-
-
-};
+module.exports = mongoose;
--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,11 @@
-var Schema = require("../db/schema.js");
+var mongoose = require("./schema");
+var seedData = require("./seeds");
+
+var Menu= mongoose.model("Menu");
+var Restaurant = mongoose.model("Restaurant");
+
+Restaurant.remove({}).then(function(){
+  Restaurant.collection.insert(seedData).then(function(){
+    process.exit();
+  });
+});
