9893f56e0849d7c89d5dd34238681bb4c52a6e4a
murphypicard <ayaghsizian@gmail.com>
2017-01-12 23:20:52 -0500

    off to a good start

--- db/schema.js
+++ db/schema.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
+
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -12,7 +13,7 @@ var Schema = mongoose.Schema;
 
 
 
-var MenuSchema = new Schema ({
+var MenuItemsSchema = new Schema ({
 	title: String
 	});
 
@@ -21,13 +22,13 @@ var RestaurantSchema = new Schema ({
 	name: String,
 	address: {street: String, zipcode: Number},
 	yelpUrl: String,
-	items: [MenuSchema]
+	items: [MenuItemsSchema]
 	});
 	
-var Restaurant = mongoose.model("Restaurant", RestaurantSchema);
-var Menu = mongoose.model("Menu", MenuSchema);
+var RestaurantModel = mongoose.model('Restaurant', RestaurantSchema);
+var MenuItemModel = mongoose.model("MenuItemsSchema", MenuItemsSchema);
 
 module.exports = {
-	Restaurant: Restaurant,
-	Menu: Menu
+	Restaurant: RestaurantModel,
+	MenuItem: MenuItemModel
 	};
